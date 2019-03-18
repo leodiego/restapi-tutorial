17c9677b611f3c1ec6c9cc88f05feae0488fee8c
Leo Fu <leo.uds@gmail.com>
2019-03-18 00:21:33 -0700

    Added Person model class

--- src/main/java/com/tutorial/restapi/model/Person.java
+++ src/main/java/com/tutorial/restapi/model/Person.java
@@ -1,4 +1,12 @@
 package com.tutorial.restapi.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Person {
+    Long id;
+    String name;
+    String age;
 }
