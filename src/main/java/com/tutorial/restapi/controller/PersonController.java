362653266b8e7ffc92c94d790a024e302a4e17b5
Leo Fu <leo.uds@gmail.com>
2019-03-18 00:20:39 -0700

    Implemented /api/person/version

--- src/main/java/com/tutorial/restapi/controller/PersonController.java
+++ src/main/java/com/tutorial/restapi/controller/PersonController.java
@@ -1,4 +1,18 @@
 package com.tutorial.restapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value={"/api/person"})
 public class PersonController {
+
+    public static final String version = "1.0";
+
+
+    @GetMapping(value={"/version", "/version/"})
+    public String version() {
+        return version;
+    }
 }
